687f492546dd05894c1a62535c689b025a36bd04
AngelAzrail <androsovdima99@gmail.com>
2022-04-04 17:41:23 +0300

    wrong format handler

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import path from "path";
 import { readFileSync } from "fs";
 import { diff, getType } from "./utils.js";
 import parsers from "./parsers/parsers.js";
+import { formatsMap } from "./format-output.js";
 
 export const prepare = (first, second, format) => {
+  if (!formatsMap[format])
+    return "Данный формат вывода не поддерживается";
   const firstFileType = getType(first);
   const secondFileType = getType(second);
-  if (!firstFileType || !secondFileType)
-    return "Невозможно прочитать файлы(ы)";
+  if (!firstFileType || !secondFileType) return "Невозможно прочитать файлы(ы)";
   const firstFile = readFileSync(path.resolve(first), "utf-8");
   const secondFile = readFileSync(path.resolve(second), "utf-8");
   const firstParsed = parsers[firstFileType](firstFile);
