951f26a415277132179ec8835201ecb2e145ed92
AngelAzrail <androsovdima99@gmail.com>
2022-04-16 18:27:16 +0300

    nested output

--- format-output.js
+++ format-output.js
@@ -10,12 +10,14 @@ export const statusesMap = {
   },
 };
 
-export const stylishFormat = (status, key, value, hasParent) =>
-  ` ${" ".repeat(hasParent)}${statusesMap[status].stylish} ${key}: ${value}\n`;
+export const stylishFormat = (status, key, value, depth) =>
+  `${" ".repeat(depth)} ${statusesMap[status].stylish} ${key}: ${value}\n`;
 
 export const formatsMap = {
   stylish: stylishFormat,
 };
 
-export const formatOutput = (status, format, key, value, hasParent) =>
-  formatsMap[format](status, key, value, hasParent);
+export const formatOutput = (status, format, key, value, depth) =>
+  formatsMap[format](status, key, value, depth);
+
+export const nestedBeginning = (status, key, depth) => `${' '.repeat(depth)} ${statusesMap[status].stylish} ${key}: {\n`
--- utils.js
+++ utils.js
@@ -1,15 +1,25 @@
 import _ from "lodash";
 import path from "path";
-import { formatOutput } from "./format-output.js";
+import {formatOutput, nestedBeginning} from "./format-output.js";
+
+export const output = (status, str, key, format, depth) => {
+  let result = "";
+  if (!_.isObject(str)) result = formatOutput(status, format, key, str, depth);
+  else {
+    result += nestedBeginning(status, key, depth);
+    Object.entries(str).forEach(([name, value]) => {
+      result += output("initial", value, name, format, depth + 2);
+    });
+    result += `${' '.repeat(depth + 2)}}\n`;
+  }
+  return result;
+};
 
 export const diff = (initial, performed, format, depth = 0) => {
   const allKeys = _.union(_.keys(initial), _.keys(performed)).sort();
   let result = "{\n";
   allKeys.forEach((key) => {
-    if (
-      (initial[key] && typeof initial[key] === "object") ||
-      (performed[key] && typeof performed[key] === "object")
-    ) {
+    if (_.isObject(initial[key]) && _.isObject(performed[key])) {
       result += formatOutput(
         "initial",
         format,
@@ -24,13 +34,13 @@ export const diff = (initial, performed, format, depth = 0) => {
       );
     } else if (!_.isEqual(initial[key], performed[key])) {
       if (initial[key] !== undefined) {
-        result += formatOutput("removed", format, key, initial[key], depth);
+        result += output("removed", initial[key], key, format, depth);
       }
       if (performed[key] !== undefined) {
-        result += formatOutput("added", format, key, performed[key], depth);
+        result += output("added", performed[key], key, format, depth);
       }
     } else {
-      result += formatOutput("initial", format, key, initial[key], depth);
+      result += output("initial", initial[key], key, format, depth);
     }
   });
   result += `${" ".repeat(depth)}}`;
